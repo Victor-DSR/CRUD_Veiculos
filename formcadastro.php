8792e6cc334d3eae051f7fa82c8b3d02f4d2ab37
mrc-broca <145123462+mrc-broca@users.noreply.github.com>
2023-12-07 21:48:02 -0300

    Add files via upload

---
+++ formcadastro.php
@@ -0,0 +1,34 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="login.css">
+    <title>Cadastro</title>
+</head>
+<body>
+
+<div class="container">
+    <div class="image-container"></div>
+    <form class="login-form" method="post" action="cadastro.php">
+        <h2>Login</h2>
+        <div class="form-group">
+            <input type="text" name="nome" placeholder="Usuário" required>
+        </div>
+        <div class="form-group">
+            <input type="password" name="senha" placeholder="Senha" required>
+        </div>
+        <div class="form-group">
+            <input type="email" name="email" placeholder="Email" required>
+        </div>
+        <button type="submit">Entrar</button>
+    </form>
+</div>
+
+</body>
+</html>
+
+<body>
+
